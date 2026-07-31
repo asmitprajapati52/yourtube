1a2a7905a080582c040d22db40980df650599b67
Asmit Prajapati <asmitprajapati52@gmail.com>
2026-07-31 21:41:51 +0530

    fix: hardcode backend url directly

--- yourtube/src/lib/axiosinstance.js
+++ yourtube/src/lib/axiosinstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Production-ready Axios instance with dynamic backend base URL
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "https://youtube-07v0.onrender.com",
+  baseURL:"https://youtube-07v0.onrender.com",
 });
 
 export default axiosInstance;
