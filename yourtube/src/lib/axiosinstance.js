10c94ceba83b450242925208cd0fd4d3ddd70f4c
Asmit Prajapati <asmitprajapati52@gmail.com>
2026-08-04 19:53:40 +0530

    fixed login

--- yourtube/src/lib/axiosinstance.js
+++ yourtube/src/lib/axiosinstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "https://youtube-07v0.onrender.com",
-  withCredentials: true, // Cookies aur headers ko properly pass karne ke liye zaroori hai
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "https://yourtube-07v0.onrender.com",
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
